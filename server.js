438c53f166b6ea2e443224b255c296e5196d045c
gabbycoloma <79184754+gabbycoloma@users.noreply.github.com>
2022-11-03 01:24:42 +0800

    Setup server

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.static(__dirname));
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
+});
