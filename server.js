7511aa9ce31e5fb4c4e7b105efb11c553c640922
gabbycoloma <79184754+gabbycoloma@users.noreply.github.com>
2022-11-19 00:42:35 +0800

    create review initial changes

--- server.js
+++ server.js
@@ -1,60 +1,104 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
-const mongoose = require("mongoose"); // mongodb 
+const mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb://localhost:27017/taste", { useNewUrlParser: true }, (err) => {
+    if (!err) console.log('DB is connected');
+    else console.log('DB Error');
+});
+
+const schema = {
+    username: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    email: String,
+    Bio: String,
+};
+
+const schema_posts = {
+    username: String,
+    date: { type: Date, default: Date.now },
+    image_post: String,
+    food_name: String,
+    restaurant_name: String,
+    stars: Number,
+    review: String,
+    likes: Number
+};
+
+const user = mongoose.model("users", schema);
+
+const posts = mongoose.model("posts", schema_posts);
 
 const app = express();
 
-// connect to database
-mongoose.connect("mongodb://localhost:27017/tasteDB", {useNewUrlParser: true}); //tasteDB name of the Database
-
 app.use(express.static("public"));
 app.use(express.static(__dirname));
-app.use(bodyparser.urlencoded({extended:true}));
-app.use(express.urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-const userSchema = { 
-  // user attributes like username, last and first name, posts
-};
-
-const posts = {
-  // posts attributes i.e. title, subtitle, date, image, post text, etc
-};
-
-
 app.get('/', function(req, res) {
-  res.render('index');
+    posts.find({}, function(err, rows) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { posts: rows });
+        }
+    });
 });
 
 app.get('/createreview', function(req, res) {
-  res.render('createreview');
+    res.render('createreview');
+});
+
+app.post('/addreview', function(req, res) {
+    const newPost = posts({
+        date: req.body.date,
+        post_image: req.body.post_image,
+        food_name: req.body.food_name,
+        restaurant_name: req.body.restaurant_name,
+        review: req.body.review
+    });
+
+    // CREATE - adding records to the DB
+    newPost.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+            console.log("added to db");
+        }
+    })
 });
 
 app.get('/editreview', function(req, res) {
-  res.render('editreview');
+    res.render('editreview');
 });
 
 app.get('/profile', function(req, res) {
-  res.render('profile');
+    res.render('profile');
 });
 
 app.get('/landingpage', function(req, res) {
-  res.render('landingpage');
+    res.render('landingpage');
 });
 
 app.get('/loginsignup', function(req, res) {
-  res.render('loginsignup');
+    res.render('loginsignup');
 });
 
 app.get('/aboutus', function(req, res) {
-  res.render('aboutus');
+    res.render('aboutus');
 });
 
 app.get('/search', function(req, res) {
-  res.render('searchresults');
+    res.render('searchresults');
 });
 
 
@@ -71,6 +115,6 @@ app.get('/search', function(req, res) {
 
 
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000.");
+app.listen(3000, function() {
+    console.log("Server is running on port 3000.");
 });
